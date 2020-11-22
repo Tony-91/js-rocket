10211fb3400d3b1da616158d82f1d88a1a14b8d4
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2020-11-22 05:20:05 -0800

    created a car and made it do things!

---
+++ explain.js
@@ -0,0 +1,28 @@
+var fullname = function (firstname, secondname) {
+	return firstname + '' + secondname;
+    }
+    
+
+var whatIsMyFavColor = function () {
+        return 'blue';
+        }
+
+
+var doSomething = function () { 
+            console.log('something!')
+        }
+
+var area = function (width, height) {
+    return width * height;
+}
+
+
+var number1 = 10;
+var number2 = 100;
+if (number1 > number2) {
+	alert ('true!');
+} else {
+    alert('false!');
+}
+
+
