ca9d737afe9c66bfaef99d160868975f0e290cf8
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2020-11-24 15:55:10 -0800

    made rocket launch!
    experiemented with randomNumber function. cool!

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-var car = {
-    make: 'VW',
-    type: 'Polo',
-    color: 'blue',
-    isTurnedOn: false,
-    numberOfWheels: 4,
-    seats: [
-        'seat 1',
-        'seat 2',
-        'seat 3',
-        'seat 4'
-    ],
-    turnOn: function () {
-        this.isTurnedOn = true;
-    },
-    fly: function () {
-        alert('fly');
-    },
-    switchCar: function (isOn) {
-        console.log('turn car ' +isOn)
-            ;
-        if (isOn == true) {
+// function to change colors by changing element's className. You tag/id the elements in HTML, you define it's properties/aesthetics in css and dynamically switch between them in js.
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+    } else {
+        document.getElementById('cool').className = 'cool'
+    }
+}
+
+    var car = {
+        make: 'VW',
+        type: 'Polo',
+        color: 'blue',
+        isTurnedOn: false,
+        numberOfWheels: 4,
+        seats: [
+            'seat 1',
+            'seat 2',
+            'seat 3',
+            'seat 4'
+        ],
+        turnOn: function () {
             this.isTurnedOn = true;
-        } else {
-            this.isTurnedOn = false;
+        },
+        fly: function () {
+            alert('fly');
+        },
+        switchCar: function (isOn) {
+            console.log('turn car ' +isOn)
+                ;
+            if (isOn == true) {
+                this.isTurnedOn = true;
+            } else {
+                this.isTurnedOn = false;
+            }
         }
     }
-};
-
-console.log('hello world')
