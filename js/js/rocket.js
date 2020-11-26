46f8b1646abdce565b624f91b663c897aba77f8b
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2020-11-26 13:04:17 -0800

    made rocket look cool!

--- js/js/rocket.js
+++ js/js/rocket.js
@@ -12,6 +12,21 @@ var changeState = function (state) {
         timer = setInterval(function () {
             countdownNumber = countdownNumber -1;
             document.getElementById('countdown').innerHTML = countdownNumber;
+
+            // be nervous
+            if (countdownNumber > 4 && countdownNumber <= 7) {
+                document.getElementById('nervous').className = 'nervous show';
+             } else{
+                document.getElementById('nervous').className = 'nervous'; 
+            }
+
+            //can't wait
+            if (countdownNumber > 1 && countdownNumber <= 4) {
+                document.getElementById('cant-wait').className = 'cant-wait show';
+            } else{
+                document.getElementById('cant-wait').className = 'cant-wait';
+            }
+
             if (countdownNumber <= 0) {
                 changeState(3);
             }
@@ -27,6 +42,7 @@ var changeState = function (state) {
                 // sucessful launch :)
                 if (randomNumber > 5) {
                     changeState(4);
+
                 // unsucessful launch :(
                 } else {
                     changeState(5);
